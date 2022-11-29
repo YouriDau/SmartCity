2e476fc30eac62c94128e6babb694972132575fa
Youri Dautrebande <100094409+YouriDau@users.noreply.github.com>
2022-11-29 14:27:58 +0100

    cors

--- API/app.js
+++ API/app.js
@@ -8,8 +8,8 @@ const InternalIp = require("internal-ip");
 const internalIP = InternalIp.v4.sync();
 
 app.use(express.json());
-app.use(Router);
 app.use(cors());
+app.use(Router);
 
 app.listen(port, internalIP, () => {
   console.log(`Server is running on adresse : http://${internalIP}:${port}`);
