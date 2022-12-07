b10ef13bbe717e4a165f9855ed6d4086aacca1cd
Youri Dautrebande <100094409+YouriDau@users.noreply.github.com>
2022-12-07 14:38:26 +0100

    routes update

--- testForm/react-component/src/routes/Routes.jsx
+++ testForm/react-component/src/routes/Routes.jsx
@@ -11,30 +11,30 @@ import ListReports from "../screen/report/ListReports";
 import UpdateReport from "../screen/report/UpdateReport";
 import UpdateReview from "../screen/review/UpdateReview";
 import DeleteReport from "../screen/report/DeleteReport";
-
+import Maps from "../screen/Maps";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"/addToilet"} element={<AddToilet/>} />
-        <Route path={"/addReport"} element={<AddReport/>} />
-        <Route path={"/addReview"} element={<AddReview/>} />
+        <Route path={"/addToilet"} element={<AddToilet />} />
+        <Route path={"/addReport"} element={<AddReport />} />
+        <Route path={"/addReview"} element={<AddReview />} />
 
-        <Route path={"/listUsers"} element={<ListUsers/>} />
-        <Route path={"/listReports"} element={<ListReports/>} />
+        <Route path={"/listUsers"} element={<ListUsers />} />
+        <Route path={"/listReports"} element={<ListReports />} />
 
-        <Route path={"/updateUser"} element={<UpdateUser/>} />
-        <Route path={"/updateUser/:id"} element={<UpdateUser/>} />
-        <Route path={"/updateReport/:id"} element={<UpdateReport/>} />
-        <Route path={"/updateReview/:id"} element={<UpdateReview/>} />
-        
-        <Route path={"/deleteUser/:id"} element={<DeleteUser/>} />
-        <Route path={"/deleteReport/:id"} element={<DeleteReport/>} />
-        
-        <Route path={"/login"} element={<Login/>} />
+        <Route path={"/updateUser"} element={<UpdateUser />} />
+        <Route path={"/updateUser/:id"} element={<UpdateUser />} />
+        <Route path={"/updateReport/:id"} element={<UpdateReport />} />
+        <Route path={"/updateReview/:id"} element={<UpdateReview />} />
+
+        <Route path={"/deleteUser/:id"} element={<DeleteUser />} />
+        <Route path={"/deleteReport/:id"} element={<DeleteReport />} />
+
+        <Route path={"/login"} element={<Login />} />
         <Route path={"/maps"} element={<Maps />} />
-        <Route path={"/"} element={<AddUser/>} />
+        <Route path={"/"} element={<AddUser />} />
       </Routes>
     </BrowserRouter>
   );
