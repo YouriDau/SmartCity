77da45713a52517c9fe28a24f3c1cf296a8037a7
Youri Dautrebande <100094409+YouriDau@users.noreply.github.com>
2022-12-19 14:06:34 +0100

    a

--- mobile/config.js
+++ mobile/config.js
@@ -1,5 +1,5 @@
 // API
-export const BASE_URL_API = "http://192.168.1.55:3001";
+export const BASE_URL_API = "http://192.168.1.53:3001";
 
 // MAP
 
--- testForm/react-component/src/config.js
+++ testForm/react-component/src/config.js
@@ -1,1 +1,1 @@
-export const BASE_URL_API = "http://192.168.1.55:3001";
+export const BASE_URL_API = "http://192.168.1.53:3001";
